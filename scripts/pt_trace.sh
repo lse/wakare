b44cd420f16890501fc2cbb32b7ec17e36978daf
Sideway <dev.sideway@gmail.com>
2019-07-11 02:21:02 +0200

    Removed return compression to prevent decoding issues (virtual stack)

--- scripts/pt_trace.sh
+++ scripts/pt_trace.sh
@@ -4,6 +4,6 @@ sudo mkdir -p /sys/fs/cgroup/cpuset/ptgroup
 echo 0 | sudo tee /sys/fs/cgroup/cpuset/ptgroup/cpuset.cpus > /dev/null
 echo 0 | sudo tee /sys/fs/cgroup/cpuset/ptgroup/cpuset.mems > /dev/null
 
-sudo cgexec -g cpuset:ptgroup perf record -e intel_pt//u --filter "filter * @ $(which $1)" $@
+sudo cgexec -g cpuset:ptgroup perf record -e intel_pt/noretcomp=1/u --filter "filter * @ $(which $1)" $@
 
 sudo chown $USER:$USER perf.data
